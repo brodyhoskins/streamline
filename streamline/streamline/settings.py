33ba2fa7be8bc4436ce1efeffca2913c887dee4a
Brody Hoskins <brody@brody.digital>
2019-12-07 22:16:37 -0800

    Revert "Create streamlineui Django app"

--- streamline/streamline/settings.py
+++ streamline/streamline/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'products',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +75,12 @@ WSGI_APPLICATION = 'streamline.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'streamline',
+        'USER': 'streamline',
+        'PASSWORD': 'seSsc2LAXNzmmszm',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
