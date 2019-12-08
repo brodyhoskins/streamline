5aa1e587b0ed8626b90038f85c451c8852c51ef8
Brody Hoskins <brody@brody.digital>
2019-12-07 18:04:45 -0800

    Use MySQL

--- streamline/streamline/settings.py
+++ streamline/streamline/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'streamline.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'streamline',
+        'USER': 'streamline',
+        'PASSWORD': 'seSsc2LAXNzmmszm',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
