196ad02e74b36e33e92cfef03cdad6a97bc57af7
Brody Hoskins <brody@brody.digital>
2020-01-15 23:52:05 -0800

    Remove redundant code

--- streamline/streamline/processors.py
+++ streamline/streamline/processors.py
@@ -4,7 +4,4 @@ def vendor(request):
     if request.user.is_authenticated:
         if Vendor.objects.filter(profile_id = request.user.profile.id):
             return { 'vendor': Vendor.objects.get(profile_id = request.user.profile.id) }
-        else:
-            return { 'vendor': None }
-    else:
-        return { 'vendor': None }
+    return { 'vendor': None }
